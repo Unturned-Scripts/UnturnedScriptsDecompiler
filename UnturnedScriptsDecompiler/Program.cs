a0af8680c7c7cfbac6f24035493d79f1304e6fec
Gamingtoday093 <Gamingtoday093@gmail.com>
2026-07-02 19:09:13 +0200

    Added BuildVersionChecker

--- UnturnedScriptsDecompiler/Program.cs
+++ UnturnedScriptsDecompiler/Program.cs
@@ -1,23 +1,65 @@
-﻿using UnturnedScriptsDecompiler.Services;
+﻿using System.Text;
+using System.Text.Json.Nodes;
+using UnturnedScriptsDecompiler.Services;
 
 namespace UnturnedScriptsDecompiler
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task<int> Main(string[] args)
         {
-            string output = "C:\\Users\\Admin\\Desktop\\OutputScripts";
-            Array.ForEach(Directory.GetDirectories(output), d => Directory.Delete(d, true));
-            Array.ForEach(Directory.GetFiles(output), File.Delete);
-            
-            ScriptDecompiler scriptDecompiler = new("C:\\Users\\Admin\\Desktop\\OutputScripts");
-            scriptDecompiler.DecompileScripts("E:\\SteamLibrary\\steamapps\\common\\Unturned\\Unturned_Data\\Managed\\Assembly-CSharp.dll");
-
-            while (true)
+            if (args.Length < 2)
             {
-                var key = Console.ReadKey();
-                if (key.KeyChar == 'q') return;
+                Console.WriteLine("You must Specify OutputPath and SteamPath");
+                return 1;
             }
+
+            string outputPath = Path.GetFullPath(args[0]);
+            if (!Directory.Exists(outputPath))
+            {
+                Console.WriteLine("OutputPath doesn't Exist!");
+                return 1;
+            }
+
+            string steamPath = Path.GetFullPath(args[1]);
+            if (!Directory.Exists(steamPath))
+            {
+                Console.WriteLine("SteamPath doesn't Exist!");
+                return 1;
+            }
+
+            bool ignoreBuildVersion = false;
+            if (args.Length >= 3 && !bool.TryParse(args[2], out ignoreBuildVersion))
+            {
+                Console.WriteLine("Unable to Parse IgnoreBuildVersion!");
+                return 1;
+            }
+
+            BuildVersionChecker buildVersionChecker = new(outputPath, steamPath);
+            if (!ignoreBuildVersion && !await buildVersionChecker.IsNewBuildAsync())
+            {
+                Console.WriteLine("Build is not Newer! Skipping..");
+                return 2;
+            }
+
+            string fileDLLPath = Path.Combine(steamPath, "Unturned_Data", "Managed", "Assembly-CSharp.dll");
+            if (!File.Exists(fileDLLPath))
+            {
+                Console.WriteLine("Assembly-CSharp.dll not Found!");
+                return 1;
+            }
+
+            ScriptDecompiler scriptDecompiler = new(outputPath);
+            scriptDecompiler.DecompileScripts(fileDLLPath);
+
+            using var stream = File.OpenRead(Path.Combine(steamPath, "Status.json"));
+            var statusGameSection = (await JsonNode.ParseAsync(stream))?["Game"];
+
+            string version = $"3.{statusGameSection?["Major_Version"] ?? '?'}.{statusGameSection?["Minor_Version"] ?? '?'}.{statusGameSection?["Patch_Version"] ?? '?'}";
+            string commitMessage = $"{DateTime.UtcNow:dd MMMM yyyy} - Version {version} ({await buildVersionChecker.GetBuildIdAsync()}){(ignoreBuildVersion ? " [Forced]" : string.Empty)}";
+            await File.WriteAllTextAsync(Path.Combine(outputPath, ".commit"), commitMessage);
+
+            return 0;
         }
     }
 }
---
+++ UnturnedScriptsDecompiler/Services/BuildVersionChecker.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ValveKeyValue;
+
+namespace UnturnedScriptsDecompiler.Services
+{
+    internal class BuildVersionChecker
+    {
+        public string OutputPath { get; }
+        public string SteamPath { get; }
+        private string? BuildId { get; set; }
+
+        public BuildVersionChecker(string outputPath, string steamPath)
+        {
+            OutputPath = outputPath;
+            SteamPath = steamPath;
+        }
+
+        public async Task<string> GetBuildIdAsync()
+        {
+            if (BuildId == null)
+                await IsNewBuildAsync(); // Populates BuildId & Updates .buildid File
+
+            return BuildId!;
+        }
+
+        public async Task<bool> IsNewBuildAsync()
+        {
+            string currentBuildIdPath = Path.Combine(OutputPath, ".buildid");
+            BuildId = await GetSteamBuildIdAsync();
+
+            if (!File.Exists(currentBuildIdPath))
+                goto newBuild;
+
+            string currentBuildId = await File.ReadAllTextAsync(currentBuildIdPath, Encoding.UTF8);
+            if (BuildId == currentBuildId) return false;
+
+        newBuild:
+            await File.WriteAllTextAsync(currentBuildIdPath, BuildId, Encoding.UTF8);
+            return true;
+        }
+
+        private async Task<string> GetSteamBuildIdAsync()
+        {
+            string appManifestPath = Path.Combine(SteamPath, "appmanifest_304930.acf");
+
+            await using var stream = File.OpenRead(appManifestPath);
+            var kv = KVSerializer.Create(KVSerializationFormat.KeyValues1Text);
+            var appManifest = kv.Deserialize(stream);
+
+            return appManifest["buildid"].ToString();
+        }
+    }
+}
